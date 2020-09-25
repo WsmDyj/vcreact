81dfc9b6b55de3ab2bbf0b385311ea070144ee3b
yushunwu <972774037@qq.com>
2020-09-25 14:34:02 +0800

    feat: 前端代码规范

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  'modules/**/*.(js|vue)': [
-    'cross-env EFF_ABSOLUTE_PATHS=true eslint --fix --format friendly',
-    'git add'
+  'src/**/*.(tsx|ts)': [
+    'eslint --ext js,ts,tsx src --max-warnings 5'
   ]
 }
