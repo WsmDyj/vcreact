e1d39d8750bc1c1e45cf085ded76fd7c344888c4
yushunwu <972774037@qq.com>
2020-09-28 18:17:54 +0800

    fix: 修改文件输出格式

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,24 +1,2 @@
-import React from 'react'
-import classNames from 'classnames'
-import { tuple } from '../_util/type'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
-library.add(fas)
-
-const ThemeProps = tuple('primary', 'default', 'danger', 'link')
-export type ThemeProp = typeof ThemeProps[number]
-
-export interface IconProps extends FontAwesomeIconProps {
-  theme?: ThemeProp | string
-}
-
-const Icon: React.FC<IconProps> = (props) => {
-  const { className, theme, ...restProps } = props
-  const classes = classNames('vc-icon', className, {
-    [`icon-${theme}`]: theme
-  })
-  return <FontAwesomeIcon className={classes} {...restProps} />
-}
-
+import Icon from './Icon'
 export default Icon
