2077ce40606b013385db072c72006a285872fff4
yushunwu <972774037@qq.com>
2020-09-28 17:51:07 +0800

    fix: 修改travis配置

---
+++ src/components/Button/button.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import Button from './button'
+
+const defaultProps = {
+  onClick: jest.fn()
+}
+
+describe('test Button component', () => {
+  it('should render the correct default button', () => {
+    const wrapper = render(<Button {...defaultProps}>Nice</Button>)
+    const element = wrapper.getByText('Nice') as HTMLButtonElement
+    expect(element.tagName).toEqual('BUTTON')
+    fireEvent.click(element)
+    expect(defaultProps.onClick).toHaveBeenCalled()
+  })
+})
