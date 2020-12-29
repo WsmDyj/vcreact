15921c46f1caf193098345a83015249790a28868
wusimin7 <972774037@qq.com>
2020-12-29 17:41:42 +0800

    chore: 前端打包上传

---
+++ storybook-static/precache-manifest.72fd16ddf70a70daa1590ea28ba761d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96af68e0a6e16e2213314e42f5a03a75",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.dd8bed3744e2cb2a5652.bundle.js"
+  },
+  {
+    "url": "runtime~main.dd8bed3744e2cb2a5652.bundle.js"
+  },
+  {
+    "revision": "d4a31d620ab52c6ad332",
+    "url": "static/css/main.9971c027.chunk.css"
+  },
+  {
+    "url": "vendors~main.dd8bed3744e2cb2a5652.bundle.js"
+  },
+  {
+    "url": "vendors~main.dd8bed3744e2cb2a5652.bundle.js.LICENSE.txt"
+  }
+]);
