bb2fb17658df92d8aad7ee8d9483a47a036d79a1
wusimin7 <wusimin7@jd.com>
2021-01-22 18:12:08 +0800

    fix: 修改目录排序方法

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,12 +7,23 @@ export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 
+const compare = (property) => {
+  return function (a, b) {
+    var value1 = a[property]
+    var value2 = b[property]
+    return value1 - value2
+  }
+}
 const loaderFn = () => {
   const menus = [require('../.storybook/welcome.stories.mdx')]
   const target = require.context('../src/components', true, /\.stories\.mdx$/)
+  const arr = []
   target.keys().forEach((fname) => {
     let index = getSortNumByMenu(fname.split('/')[1])
-    menus.splice(index, 0, target(fname))
+    arr.push({ stories: fname, id: index})
+  })
+  arr.sort(compare('id')).forEach(item => {
+    menus.push(target(item.stories))
   })
   return menus
 }
