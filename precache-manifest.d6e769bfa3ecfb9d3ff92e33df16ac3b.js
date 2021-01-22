891639fba73a56926708b2118e6b9fc681bb82b9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-22 10:15:18 +0000

    Deploy WsmDyj/vcreact to github.com/WsmDyj/vcreact.git:gh-pages

---
+++ precache-manifest.d6e769bfa3ecfb9d3ff92e33df16ac3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd43ed9d72549ad47f4eb71c0398ce07",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.36f514d03d3286ab4455.bundle.js"
+  },
+  {
+    "url": "runtime~main.36f514d03d3286ab4455.bundle.js"
+  },
+  {
+    "revision": "1ab665954c827d336552",
+    "url": "static/css/main.9971c027.chunk.css"
+  },
+  {
+    "url": "vendors~main.36f514d03d3286ab4455.bundle.js"
+  },
+  {
+    "url": "vendors~main.36f514d03d3286ab4455.bundle.js.LICENSE.txt"
+  }
+]);
